08e3954da310c3d779b2cce66619741096202b59
Nate Klein <32604753+nateklein@users.noreply.github.com>
2019-07-09 13:37:54 -0400

    Update coords for beam

--- full_auto_sim.py
+++ full_auto_sim.py
@@ -2,27 +2,32 @@
 # Written by Nate Klein
 
 import ScriptEnv
+
+# Init stuff
 ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
 oDesktop.RestoreWindow()
 oProject = oDesktop.SetActiveProject("BPM Sim")
 oDesign = oProject.SetActiveDesign("Maxwell3DDesign1")
 oEditor = oDesign.SetActiveEditor("3D Modeler")
 
-coords = [(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0)]
+# Coordinates to take measurements at
+coords = [(0.0, 0.0)]
 
-data = open("sim_data.csv", 'w')
+data = open("sim_data.csv", 'w+')
 
+# For every coordinate
 for i in coords:
+
 	# Make the beam
 	oEditor.CreateCylinder(
 		[
 			"NAME:CylinderParameters",
-			"XCenter:="		, "50mm",
-			"YCenter:="		, str(i[0])+"mm",
-			"ZCenter:="		, str(i[1])+"mm",
+			"XCenter:="		, str(i[0])+"mm",
+			"YCenter:="		, str(i[1])+"mm",
+			"ZCenter:="		, "50mm",
 			"Radius:="		, "2mm",
 			"Height:="		, "-100mm",
-			"WhichAxis:="		, "X",
+			"WhichAxis:="		, "Z",
 			"NumSides:="		, "0"
 		], 
 		[
@@ -52,18 +57,20 @@ for i in coords:
 
 	# Run analysis
 	oDesign.AnalyzeAll()
-	oModule = oDesign.GetModule("FieldsReporter")
-	oModule.ExportMarkerTable("//Client/E$/SRP/IREAP 2019/voltages.csv")
+
+	# Pull voltage data
+	# oModule = oDesign.GetModule("FieldsReporter")
+	# oModule.ExportMarkerTable("//Client/E$/SRP/IREAP 2019/voltages_at_"+str(i[0])+"_"+str(i[1])+".csv")
 
 	# Write data to file
-	f = open("//Client/E$/SRP/IREAP 2019/voltages.csv", 'r')
-	f.readline()
-	voltages = []
-	while f.readline() != "":
-		voltages += f.readline().split(",")[4]
-	f.close()
+	# f = open("//Client/E$/SRP/IREAP 2019/voltages.csv", 'r')
+	# f.readline()
+	# voltages = []
+	# while f.readline() != "":
+	# 	voltages += f.readline().split(",")[4]
+	# f.close()
 
-	data.write(str(i[0])+","+str(i[1])+","+",".join(voltages))
+	# data.write(str(i[0])+","+str(i[1])+","+",".join(voltages))
 
 	# Delete the beam
 	oEditor.Delete(
